2a3f76204eb372e9b52390b5ea093447da1f279e
sadra soleimani <sadrasadrasadra6@gmail.com>
2024-09-27 14:01:07 +0330

    build: abstract database module

---
+++ libs/database/src/database.module.ts
@@ -0,0 +1,19 @@
+import { Module } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { ModelDefinition, MongooseModule } from "@nestjs/mongoose";
+
+@Module({
+    imports: [
+        MongooseModule.forRootAsync({
+            useFactory: (configService: ConfigService) => ({
+                uri: configService.get<string>("MONGODB_URI_PRODUCT")
+            }),
+            inject: [ConfigService]
+        })
+    ]
+})
+export class DatabaseModule {
+    static forFeature(models: ModelDefinition[]) {
+        return MongooseModule.forFeature(models);
+    }
+}
