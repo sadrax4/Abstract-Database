e4256732d14c62f2f553a424877441622b3c0e77
sadra soleimani <sadrasadrasadra6@gmail.com>
2024-09-27 14:03:53 +0330

    import: database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
+import { DatabaseModule } from '@app/database/database.module';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [DatabaseModule],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
